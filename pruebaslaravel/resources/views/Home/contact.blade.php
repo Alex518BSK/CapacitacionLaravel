fd5c9321b0319661de16ea6138e0795bd800740f
Alejandro <alex518.bsk@gmail.com>
2017-06-03 13:25:12 -0500

    uso de los seccion para la maquetacion con la app

---
+++ pruebaslaravel/resources/views/Home/contact.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<div class="content">
+<p>Hola</p>
+</div>
+@endsection()
+
+@section('footer')
+<p>footer</p>
+@endsection()
--- pruebaslaravel/routes/web.php
+++ pruebaslaravel/routes/web.php
@@ -12,38 +12,46 @@ use Illuminate\Support\facades\cache;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/more/5/perrito', function () {
-//     return View::make('about');
-// });
-// Route::get('/more/{id}/{name}', function ($id,$name) {
-//     return "este es un numero ". $id." ".$name;
-// });
-// Route::get('admin/poster/example',array('as' => 'admin.home', function(){
-//     $url=route('admin.home');
-//     return "this url is ". $url;
-// }));
-// Route::get('about-page',function(){
-//     return view('other.about');
-// })->name('other.about');
-// Route::get('/cache', function() {
-//     return Cache::get('key');
-// });
-// Route::group(['prefix'=>'admins'],function(){
-//     Route::get('',function(){
-//         return view('about');
-//     })->name('admin.index');
-    
-//     Route::get('pruebados', function() {
-//         return view('about');
-//     })->name("admin.prueba2");
+Route::get('/hogar', function () {
+    return view('Home.home');
+});
 
-//     Route::get('blog/{id}/{nombre}/{edad}', 'blog\CommentController@index' )->name("admin.blog");
+Route::get('/more/5/perrito', function () {
+    return View::make('about');
+});
+Route::get('/more/{id}/{name}', function ($id,$name) {
+    return "este es un numero ". $id." ".$name;
+});
+Route::get('admin/poster/example',array('as' => 'admin.home', function(){
+    $url=route('admin.home');
+    return "this url is ". $url;
+}));
+Route::get('about-page',function(){
+    return view('other.form');
+})->name('other.about');
+Route::post('about-page',function(){
+    return view('other.form');
+})->name('other.aboutpost');
+
+Route::get('/cache', function() {
+    return Cache::get('key');
+});
+Route::group(['prefix'=>'admins'],function(){
+    Route::get('',function(){
+        return view('about');
+    })->name('admin.index');
     
-// });
+    Route::get('pruebados', function() {
+        return view('about');
+    })->name("admin.prueba2");
+
+    Route::get('blog/{id}/{nombre}/{edad}', 'blog\CommentController@index' )->name("admin.blog");
+    
+});
 
 Route::resource('bloger', 'blog\CommentController');
 
